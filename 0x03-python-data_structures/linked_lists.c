470d0864c4741144fa780d39bd7076b18a585eee
Aniekwe Onyekachi Godwin <aniekweonyekachi@gmail.com>
2023-08-15 13:07:46 +0000

    updated

--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -1,34 +1,34 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
 /**
- * print_listint - prints elements of a listint_t list
- * @h: ptr to head of list
- * Return: numb of nodes
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
     const listint_t *current;
-    unsigned int n; /* numb (number) of nodes */
+    unsigned int n; /* number of nodes */
 
     current = h;
     n = 0;
     while (current != NULL)
     {
-	printf("%i\n", current->n);
-	current = current->next;
-	n++;
+        printf("%i\n", current->n);
+        current = current->next;
+        n++;
     }
 
     return (n);
 }
 
 /**
- * add_nodeint_end - adds new node at end of a list_int_t list
- * @head: ptr to ptr of the first node of list_int_t list
- * @n: the int to be included in a new node
- * Return: addr of the new element/NULL if it fails
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @n: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
@@ -39,26 +39,26 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
     new = malloc(sizeof(listint_t));
     if (new == NULL)
-	return (NULL);
+        return (NULL);
 
     new->n = n;
     new->next = NULL;
 
     if (*head == NULL)
-	*head = new;
+        *head = new;
     else
     {
-	while (current->next != NULL)
-	    current = current->next;
-	current->next = new;
+        while (current->next != NULL)
+            current = current->next;
+        current->next = new;
     }
 
     return (new);
 }
 
 /**
- * free_listint - frees the list_int_t list
- * @head: ptr to list to be freed
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
  * Return: void
  */
 void free_listint(listint_t *head)
@@ -67,8 +67,8 @@ void free_listint(listint_t *head)
 
     while (head != NULL)
     {
-	current = head;
-	head = head->next;
-	free(current);
+        current = head;
+        head = head->next;
+        free(current);
     }
 }
