2a6f70e56b477f94c42130eaec9ecad1a6787438
Aniekwe Onyekachi Godwin <aniekweonyekachi@gmail.com>
2023-08-30 15:27:03 -0700

    A class Square that defines a square based on 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines class Square"""
+
+
+class Square:
+    """Represents the square"""
+
+    def __init__(self, size):
+        """Initializes the new Square
+
+        Args:
+            size (int): Size of the new square
+        """
+        self.__size = size
