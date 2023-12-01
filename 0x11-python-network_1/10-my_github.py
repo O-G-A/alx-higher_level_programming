f8fe983924e0398514c8a5cd5a75406c806d7de1
Aniekwe Onyekachi Godwin <aniekweonyekachi@gmail.com>
2023-12-01 20:54:00 +0100

    A Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id.

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
-"""Displays value of the X-Request-Id variable found in 
-header of the response.
+"""
+Python script that takes your Github credentials (username and password) 
+then displays your id using Github API.
 """
 
-if __name__ == "__main__":
-    from requests import get
-    from sys import argv
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
 
-    r = get('https://api.github.com/user', auth=(argv[1], argv[2]))
+if __name__ == '__main__':
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+    r = requests.get(url,
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
     print(r.json().get('id'))
